7fedb1c71c2a1cbdbc9100b7bf4430176e8a3bf8
Wowsaruss <russell.hayes.dev@gmail.com>
2025-06-01 14:16:46 -0700

    boards and lists apis

--- lib/todo_backend/router.ex
+++ lib/todo_backend/router.ex
@@ -1,33 +1,80 @@
 defmodule TodoBackend.Router do
   use Plug.Router
 
+  alias TodoBackend.{
+    UserController,
+    BoardController,
+    ListController,
+    TodoController
+  }
+
   plug :match
+  plug Plug.Parsers,
+    parsers: [:json],
+    pass: ["application/json"],
+    json_decoder: Jason
   plug :dispatch
 
+  # User routes
   get "/users" do
-    TodoBackend.UserController.index(conn)
+    UserController.index(conn)
   end
   get "/users/:id" do
-    id = String.to_integer(id)
-    TodoBackend.UserController.show(conn, id)
+    UserController.show(conn, %{"id" => id})
   end
   post "/users" do
-    {:ok, body, conn} = read_body(conn)
-    params = Jason.decode!(body)
-    TodoBackend.UserController.create(conn, params)
+    UserController.create(conn, conn.body_params)
   end
 
-  get "/todos" do
-    TodoBackend.TodoController.index(conn)
+  # Board routes
+  get "/boards" do
+    BoardController.index(conn)
   end
-  get "/todos/:id" do
-    id = String.to_integer(id)
-    TodoBackend.TodoController.show(conn, id)
+  get "/boards/:id" do
+    BoardController.show(conn, %{"id" => id})
+  end
+  post "/boards" do
+    BoardController.create(conn, conn.body_params)
+  end
+  put "/boards/:id" do
+    BoardController.update(conn, Map.put(conn.body_params, "id", id))
+  end
+  delete "/boards/:id" do
+    BoardController.delete(conn, %{"id" => id})
+  end
+
+  # List routes
+  get "/boards/:board_id/lists" do
+    ListController.index(conn)
+  end
+  post "/boards/:board_id/lists" do
+    ListController.create(conn, Map.put(conn.body_params, "board_id", board_id))
+  end
+  get "/lists/:id" do
+    ListController.show(conn, %{"id" => id})
+  end
+  put "/lists/:id" do
+    ListController.update(conn, Map.put(conn.body_params, "id", id))
+  end
+  delete "/lists/:id" do
+    ListController.delete(conn, %{"id" => id})
+  end
+
+  # Todo routes
+  get "/todos" do
+    TodoController.index(conn)
   end
   post "/todos" do
-    {:ok, body, conn} = read_body(conn)
-    params = Jason.decode!(body)
-    TodoBackend.TodoController.create(conn, params)
+    TodoController.create(conn, conn.body_params)
+  end
+  get "/todos/:id" do
+    TodoController.show(conn, %{"id" => id})
+  end
+  put "/todos/:id" do
+    TodoController.update(conn, Map.put(conn.body_params, "id", id))
+  end
+  delete "/todos/:id" do
+    TodoController.delete(conn, %{"id" => id})
   end
 
   match _ do
