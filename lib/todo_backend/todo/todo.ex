95d1864c0fe91bd00f7efb4b2fd496be26054d93
Wowsaruss <russell.hayes.dev@gmail.com>
2025-06-01 11:18:48 -0700

    todo changeset updates

--- lib/todo_backend/todo/todo.ex
+++ lib/todo_backend/todo/todo.ex
@@ -2,15 +2,26 @@ defmodule TodoBackend.Todo do
   use Ecto.Schema
   import Ecto.Changeset
 
-  @derive {Jason.Encoder, only: [:id, :title, :completed]}
+  @derive {Jason.Encoder, only: [:id, :title, :order, :completed, :user_id, :list_id, :description, :due_date, :position, :inserted_at, :updated_at]}
   schema "todos" do
     field :title, :string
+    field :order, :integer
     field :completed, :boolean, default: false
+    field :description, :string
+    field :due_date, :utc_datetime
+    field :position, :integer
+
+    belongs_to :user, TodoBackend.User
+    belongs_to :list, TodoBackend.List
+
+    timestamps(type: :utc_datetime)
   end
 
   def changeset(todo, attrs) do
     todo
-    |> cast(attrs, [:title, :completed])
-    |> validate_required([:title])
+    |> cast(attrs, [:title, :order, :completed, :user_id, :list_id, :description, :due_date, :position])
+    |> validate_required([:title, :user_id])
+    |> foreign_key_constraint(:user_id)
+    |> foreign_key_constraint(:list_id)
   end
 end
