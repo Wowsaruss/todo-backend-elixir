86e8dd32d076cc6f920bba65547354c32eafa9dd
Wowsaruss <russell.hayes.dev@gmail.com>
2025-06-01 14:16:16 -0700

    errors function

--- lib/todo_backend.ex
+++ lib/todo_backend.ex
@@ -2,4 +2,12 @@ defmodule TodoBackend do
   @moduledoc """
   TodoBackend is a simple REST API for managing todo items.
   """
+
+  def changeset_errors(changeset) do
+    Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
+  end
 end
